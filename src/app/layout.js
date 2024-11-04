fb69954d044fc8fc38e85f1973dbe5babb445b72
Shubham Shinde <70743278+shubhamshnd@users.noreply.github.com>
2024-11-04 15:01:12 +0530

    Update layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,6 @@
 import localFont from "next/font/local";
 import "./globals.css";
-
+import { Analytics } from "@vercel/analytics/react"
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
   variable: "--font-geist-sans",
